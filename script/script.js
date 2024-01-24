476821f59903cffbf08a14ec27c82a124d456394
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-01-24 19:30:38 +0100

    Added a Grid Size
    Added a HTML input
    Added class for square 10 50 100
    Added a JS script to select the grid choice

--- script/script.js
+++ script/script.js
@@ -4,24 +4,51 @@ const controlButtonRainbow = document.querySelector(".rainbow");
 const controlButtonErase = document.querySelector(".erase");
 const controlButtonClear = document.querySelector(".clear");
 const gridSketchContainer = document.querySelector("#sketch-grid");
+const userGridSelection = document.querySelector("#grid-selection")
+
+//The value from the user input is stored in a variable.
+//Every time the input changes, the value is changed.
+//The value is used to modify the class while creating the div.
+//The value is used to modify the gridsize.
+//Everytime the input is modified the grid changes.
 
 
+let gridSize = +userGridSelection.value
+userGridSelection.addEventListener("input", () => {
+    gridSize = +userGridSelection.value;
+    makeGrid(gridSize)
+});
 
 // Dynamic DIV and GRID setup with square class
 const makeDiv = () => {
     const div = document.createElement("div");
-    div.classList.add("square")
-    return div;
+    if (gridSize===10){
+        div.classList.add("square-10");
+        div.classList.remove("square-50");
+        div.classList.remove("square-100");
+        return div;
+    } else if (gridSize===50){
+        div.classList.remove("square-10");
+        div.classList.add("square-50");
+        div.classList.remove("square-100");
+        return div;
+    } else if (gridSize===100){
+        div.classList.remove("square-10");
+        div.classList.remove("square-50");
+        div.classList.add("square-100");
+        return div;
+    }
 }
 
-const makeGrid =() => {
-    for (let i = 0 ; i < 10 ; i++){
-        for (let j = 0 ; j < 10 ; j++){
+const makeGrid = (value) => {
+    for (let i = 0 ; i < value ; i++){
+        for (let j = 0 ; j < value ; j++){
         gridSketchContainer.appendChild(makeDiv());
         }
     }
 }
-makeGrid();
+
+makeGrid(gridSize);
 
 //Track of boolean value for buttons
 let isRainbowMode=false;
@@ -39,7 +66,8 @@ controlButtonBlack.addEventListener("click", () => {
 
 
 // Function to switch black hovering / clicking
-const squares = document.querySelectorAll(".square");
+
+let squares = document.querySelectorAll(".square-10");
 squares.forEach(square => {
     square.addEventListener("mouseover", () => {
         if (isRainbowMode===true){
